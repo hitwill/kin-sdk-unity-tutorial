a60ace72b19f199c9dda6756ab38b160c701e8ad
Will Gikandi <kawess@gmail.com>
2019-07-11 12:53:00 +0700

    initial commit

---
+++ Assets/Scripts/Tutorial.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Tutorial : MonoBehaviour
+{
+
+    
+   private KinWrapper kinWrapper;
+    
+   public Text textBalance;
+
+   public void SendKin(){
+       //send Kin to server
+       string memo = "tutorial Earn";
+       kinWrapper.SendKin(5m, memo);
+    }
+
+   public void RequestKin(){
+        //request Kin from server
+        string memo = "tutorial Earn";
+        kinWrapper.EarnKin(10m, memo);
+   }
+
+    void ListenKin(object eventData, string type)
+    {
+        if(type == "balance"){
+            textBalance.text = PlayerPrefs.GetFloat("KinBalanceUser", 0).ToString();
+        }
+
+        GameObject.Find("TutorialLog").GetComponent<Text>().text +=  "\n" +
+            eventData.ToString();
+    }
+
+    void Start()
+    {
+        string url = "https://smite-back-end.herokuapp.com";
+        string serverAddress = "GAFWSBEOGCCYVEC5YZUILDEDGHO27PODVTQJ45DSFBODKRXQ42MVLIZZ";
+        kinWrapper = GameObject.Find("KinWrapper").GetComponent<KinWrapper>();
+        kinWrapper.Initialize(ListenKin, url, serverAddress);
+    }
+
+    
+}
